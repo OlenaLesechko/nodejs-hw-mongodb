7471885c9f9a6abfe762d63540cad0cf88fbd773
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-07-02 19:46:12 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,24 +1,49 @@
-import { Contact } from "../db/contact.js"; 
+import express from 'express';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
-export const getAllContacts = async () => {
+const app = express();
+
+app.get('/contacts', async (req, res, next) => {
     try {
-        const contacts = await Contact.find();
-        return contacts;
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            status: 200,
+            message: 'Successfully retrieved contacts',
+            data: contacts
+        });
     } catch (error) {
-        throw new Error(`Unable to retrieve contacts: ${error.message}`);
+        next(error);
     }
-    };
+});
 
-    export const getContactById = async (contactId) => {
+
+app.get('/contacts/:id', async (req, res, next) => {
+    const { id } = req.params;
+
     try {
-        const contact = await Contact.findById(contactId);
-        if (!contact) {
-        throw new Error('Contact not found');
-        }
-        return contact;
+        const contact = await getContactById(id);
+        res.status(200).json({
+            status: 200,
+            message: `Successfully retrieved contact with ID ${id}`,
+            data: contact
+        });
     } catch (error) {
-        throw new Error(`Unable to retrieve contact by ID: ${error.message}`);
+        next(error);
     }
-};
+});
 
 
+app.use((err, req, res, ) => {
+    console.error(err);
+    res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+        error: err.message
+    });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
